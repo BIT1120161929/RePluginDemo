84ee48361594758f55e637367f5ca4f94588fac2
卿思九 <qingsijiu@up366.com>
2019-12-12 14:56:21 +0800

    增加了插件使用宿主的类的方法

--- myplugin/app/src/main/java/com/example/myplugin/MainActivity.java
+++ myplugin/app/src/main/java/com/example/myplugin/MainActivity.java
@@ -6,17 +6,18 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.myplugin.databinding.ActivityMainBinding;
 import com.qihoo360.replugin.RePlugin;
 
-import java.util.logging.Logger;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -68,25 +69,59 @@ public class MainActivity extends AppCompatActivity {
             DataHolder dataHolder = new DataHolder((position + 1) + "");
             ((TextRecyclerView) (holder.itemView)).setText("这是第" + dataHolder.string + "个功能");
             ((TextRecyclerView) (holder.itemView)).seButton("这是第" + dataHolder.string + "个功能按钮");
-            if (position == 0) {
-                ((TextRecyclerView) (holder.itemView)).binding.btnFunction.setOnClickListener(view -> {
-                    Log.e("Plugin", "点击了按钮希望回到宿主的MainActivity");
-                    Intent intent = RePlugin.createIntent("com.example.myhost","com.example.myhost.MainActivity");
-                    if(intent==null)return;
-                    /**
-                     * 这就是个flag就是Activity的四种启动模式……
-                     * 估计是因为plugin2 的点击事件是在Activity里写的，所以startActivity调用的是被Activity重写过的stratActivity。
-                     */
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    view.getContext().startActivity(intent);
-                });
-            } else if (position == 1) {
-                ((TextRecyclerView) (holder.itemView)).binding.btnFunction.setOnClickListener(view -> {
-                    Log.e("Plugin", "点击了按钮希望进入测试页面");
-                    Intent intent = new Intent(view.getContext(),TestActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    view.getContext().startActivity(intent);
-                });
+
+            switch (position) {
+                case 0:
+                    ((TextRecyclerView) (holder.itemView)).setText("回到宿主工程MainActivity");
+                    ((TextRecyclerView) (holder.itemView)).seButton("跳转");
+                    ((TextRecyclerView) (holder.itemView)).binding.btnFunction.setOnClickListener(view -> {
+                        Intent intent = RePlugin.createIntent("com.example.myhost", "com.example.myhost.MainActivity");
+                        if (intent == null) return;
+                        /**
+                         * 这就是个flag就是Activity的四种启动模式……
+                         * 估计是因为plugin2 的点击事件是在Activity里写的，所以startActivity调用的是被Activity重写过的stratActivity。
+                         */
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        view.getContext().startActivity(intent);
+                    });
+                    break;
+                case 1:
+                    ((TextRecyclerView) (holder.itemView)).setText("使用宿主的类");
+                    ((TextRecyclerView) (holder.itemView)).seButton("使用并替换标题");
+                    ((TextRecyclerView) (holder.itemView)).binding.btnFunction.setOnClickListener(view->{
+                        ClassLoader hostClassLoader = RePlugin.getHostClassLoader();
+                        ((TextRecyclerView) (holder.itemView)).seButton("点击了按钮");
+                        try {
+                            if(hostClassLoader!=null){
+                                Class hostTestClass = hostClassLoader.loadClass("com.example.myhost.TestClass");
+                                Object testClass = hostTestClass.newInstance();
+                                Method getName = hostTestClass.getDeclaredMethod("getName");
+                                Method getDescription = hostTestClass.getDeclaredMethod("getDescription");
+                                Method setName = hostTestClass.getDeclaredMethod("setName",String.class);
+                                Method setDescription = hostTestClass.getDeclaredMethod("setDescription",String.class);
+
+                                setName.invoke(testClass,"testName");
+                                setDescription.invoke(testClass,"testDescription");
+                                if(testClass!=null){
+                                    ((TextRecyclerView) (holder.itemView)).setText(getName.invoke(testClass) + "   "+getDescription.invoke(testClass));
+                                }
+                            }else{
+                                ((TextRecyclerView) (holder.itemView)).setText("Class加载失败");
+                            }
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                            ((TextRecyclerView) (holder.itemView)).setText("ClassNotFoundException");
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (InstantiationException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchMethodException e) {
+                            e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    break;
             }
         }
 
